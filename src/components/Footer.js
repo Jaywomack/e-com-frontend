e4ef9b6856ea46cae9c466e38783390f08002b6d
jaywomack <womackjw@hotmail.com>
2021-05-11 14:02:27 -0700

    Created Product cards and HomeScreen bringing in Dummy Data to fill cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 function Footer() {
-    return(
-        <div>
-            <footer>Footer</footer>
-        </div>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
